655016b0ce8ebfb3954986abadd8d2e6c30cbec8
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-08-15 17:25:39 -0300

    'menu'

--- src/components/FooterBlog/index.tsx
+++ src/components/FooterBlog/index.tsx
@@ -2,7 +2,8 @@ import styles from './styles.module.scss'
 import Link from 'next/link'
 import { AiFillPhone } from 'react-icons/ai';
 import { GrMail } from 'react-icons/gr'
-import { FaMapMarker } from 'react-icons/fa'
+import { FaMapMarker, FaGithubSquare } from 'react-icons/fa'
+import { AiFillInstagram, AiFillLinkedin, AiFillFacebook } from 'react-icons/ai'
 
 export function FooterBlog() {
 
@@ -12,35 +13,72 @@ export function FooterBlog() {
       <footer className={styles.footerContainer}>
          <section className={styles.sectionContentFooter}>
             <div className={styles.contentFooter}>
-               <h3>Sobre</h3>
+               <h2>Sobre</h2>
                <hr />
                <br />
                <br />
                <Link href="/">
-                  <img src="LogoBuilderWhite.png" width={170} height={50} />
+                  <img src="LogoBuilderWhite.png" width={190} height={70} />
                </Link>
                <br />
                <br />
-               <p>Insira aqui informações úteis sobre a seu negócio, como uma breve descrição do que você faz e meios de contato.</p>
+               <p>Desenvolvemos ferramentas e soluções online para o seu negócio.</p>
+               <br />
                <br />
                <br />
                <div className={styles.contactsFooter}>
-                  <p><AiFillPhone color='var(--orange)' size={25} /> (54) 99686-0104</p>
-                  <p><GrMail color='var(--orange)' size={25} /> contato@builderseunegocioonline.com.br</p>
-                  <p><FaMapMarker color='var(--orange)' size={25} /> Rua José Soares de Oliveira 2417 Caxias do Sul-RS CEP: 95034-100</p>
+                  <div className={styles.containerContacts}>
+                  <span><AiFillPhone color='var(--orange)' size={38} /></span>
+                  <p> (54) 99686-0104</p>
+                  </div>
+                  <div className={styles.containerContacts}>
+                  <span><GrMail color='var(--orange)' size={38} /></span>
+                  <p> contato@builderseunegocioonline.com.br</p>
+                  </div>
+                  <div className={styles.containerContacts}>
+                  <span><FaMapMarker color='var(--orange)' size={38} /></span>
+                  <p>Rua José Soares de Oliveira 2417 Caxias do Sul-RS CEP: 95034-100</p>
+                  </div>
                </div>
             </div>
             <div className={styles.contentFooter}>
-               <h3>Postagens Recentes</h3>
+               <h2>Postagens Recentes</h2>
                <hr />
+
             </div>
             <div className={styles.contentFooter}>
-               <h3>Menu</h3>
+               <h2>Menu</h2>
                <hr />
+               <br />
+               <br />
+               <Link href="/">
+                  <a>Inicio</a>
+               </Link>
+               <a>Categorias</a>
+               <Link href="https://builderseunegocioonline.com.br" target="_blank">
+                  <a>Nossos Serviços</a>
+               </Link>
+               <Link href="/sobre">
+                  <a>Sobre</a>
+               </Link>
             </div>
             <div className={styles.contentFooter}>
-               <h3>Redes Sociais</h3>
+               <h2>Redes Sociais</h2>
                <hr />
+               <br/>
+               <br/>
+               <Link href="https://www.facebook.com/builderseunegocioonline" target="_blank">
+                  <AiFillFacebook color='var(--orange)' size={45}/>
+               </Link>
+               <Link href="https://www.instagram.com/builderseunegocioonline" target="_blank">
+                  <AiFillInstagram color='var(--orange)' size={45}/>
+               </Link>
+               <Link href="https://www.linkedin.com/in/gabriel-campos-de-bastiani" target="_blank">
+                  <AiFillLinkedin color='var(--orange)' size={45}/>
+               </Link>
+               <Link href="https://github.com/gabrielbastiani" target="_blank">
+                  <FaGithubSquare color='var(--orange)' size={45}/>
+               </Link>
             </div>
          </section>
 
--- src/components/HeaderBlog/index.tsx
+++ src/components/HeaderBlog/index.tsx
@@ -1,30 +1,32 @@
 import styles from './styles.module.scss'
 import Link from 'next/link'
 
-export function HeaderBlog(){
+export function HeaderBlog() {
 
 
-   return(
+   return (
       <header className={styles.headerContainer}>
          <div className={styles.headerContent}>
             <Link href="/">
                <img src="LogoBuilderWhite.png" width={170} height={50} />
             </Link>
-            
+
             <nav className={styles.menuNav}>
-               <Link href="/">
-                  <a>Inicio</a>
-               </Link>
-
-               <a>Categorias</a>
-
-               <Link href="https://builderseunegocioonline.com.br" target="_blank">
-                  <a>Nossos Serviços</a>
-               </Link>
-
-               <Link href="/sobre">
-                  <a>Sobre</a>
-               </Link>
+               <ul>
+                  <li><Link href="/"><a>Inicio</a></Link></li>
+                  <li><a>Categorias</a></li>
+                     <ul className={styles.submenu}>
+                        <li><a>hghghghghg</a></li>
+                        <li><a>hghghghghg</a></li>
+                        <li><a>hghghghghg</a></li>
+                        <li><a>hghghghghg</a></li>
+                        <li><a>hghghghghg</a></li>
+                        <li><a>hghghghghg</a></li>
+                        <li><a>hghghghghg</a></li>
+                     </ul>
+                  <li><Link href="https://builderseunegocioonline.com.br" target="_blank"><a>Nossos Serviços</a></Link></li>
+                  <li><Link href="/sobre"><a>Sobre</a></Link></li>
+               </ul>
             </nav>
          </div>
       </header>
